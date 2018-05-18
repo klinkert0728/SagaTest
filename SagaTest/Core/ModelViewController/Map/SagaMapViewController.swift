4260b846b7604554d95b4c56abeb8d2cbfe5e53a
klinkert0728 <dklinker@eafit.edu.co>
2018-05-18 14:12:33 -0500

    remove title

--- SagaTest/Core/ModelViewController/Map/SagaMapViewController.swift
+++ SagaTest/Core/ModelViewController/Map/SagaMapViewController.swift
@@ -23,7 +23,6 @@ class SagaMapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Saga Test"
         configureMap()
         setupLocationManager()
         // Do any additional setup after loading the view.
